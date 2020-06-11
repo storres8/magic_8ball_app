8dc3a32e6e18615aa7268ebaacadd3ce755f36f2
Steven Torres <stevetorres7562@gmail.com>
2020-06-10 22:24:25 -0400

    built main storyboard1

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    let ballArray:[UIImage] = [#imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: <#T##String#>)]
 
 
 }
